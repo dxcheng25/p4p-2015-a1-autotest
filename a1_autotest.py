9529c9f7b5b9ff003702555a2ed64fe49d9b29e9
David Xi Cheng <david.cheng@uwaterloo.ca>
2015-02-05 12:35:02 -0500

    eliminate phantom no output err, execute each executable five times with random number of threads.

--- a1_autotest.py
+++ a1_autotest.py
@@ -1,13 +1,23 @@
-import os, sys, glob, random
+import os, sys, glob, random, shutil
 import assign_builder, paster_exec, png_comparator
 from result_logger import *
+
+tester_root = os.path.abspath( os.path.dirname(__file__) )
  
 def a1_autotest(path_to_userids):	
 	#grab all users' repo roots
 	pus = glob.glob( path_to_userids+'/*' )
 	output_file = result_logger('autotest_all.csv')
 
+	if not os.path.exists('wrong_output'):
+		os.mkdir('wrong_output')
+
+	if not os.path.exists('tmp_output'):
+		os.mkdir('tmp_output')
+
 	for pu in pus:
+		#get absolute path
+		pu = os.path.abspath(pu)
 		#grep userid
 		uid = os.path.basename( pu )
 		#.../userid/a1/
@@ -37,26 +47,40 @@ def a1_autotest(path_to_userids):
 		exe_nbio = 'paster_nbio'
 
 		#ignore -i argument due to an error in the provided source code
-		#for i in range(1,4):
-		#.../userid/a1/bin/paster_parallel -i[1:3]
-		execute_paster( pua1bin+exe_para, exe_para, 1, output_file, 30)
-		remove_output()
+		os.chdir( 'tmp_output' )
 
-		#.../userid/a1/bin/paster_nbio -i[1:3]
-		execute_paster( pua1bin+exe_nbio, exe_nbio, 1, output_file, 30)
-		remove_output()
+		for i in range(1,6):
+			#.../userid/a1/bin/paster_parallel -i[1:3]
+			execute_paster( uid, pua1bin+exe_para, exe_para, 1, output_file, 30)
+			remove_output( '.' )
+
+		for i in range(1,6):
+			#.../userid/a1/bin/paster_nbio -i[1:3]
+			execute_paster( uid, pua1bin+exe_nbio, exe_nbio, 1, output_file, 30)
+			remove_output( '.' )
+	
+		os.chdir( '..' )
 
 	output_file.logger_close()
 
 
-def remove_output():
-	if os.path.exists('output.png'):
+def remove_output(output_dir=None):
+	if output_dir:
+		files = glob.glob(output_dir+'/*')
+		for f in files:
+			os.remove(f)
+	
+	elif os.path.exists('output.png'):
 		os.remove('output.png')
 
 
-def execute_paster(pu_exe, exe_name, img_seq, output_file, timeout):
-	#number of t is randomly generated within [4,20]
-	num_t = random.randrange(4, 16)
+def copy_wrong_output(src, userid, exe_name):
+	shutil.copyfile( src, '../wrong_output/'+userid+'_'+exe_name+'_'+'output.png' )
+
+
+def execute_paster(userid, pu_exe, exe_name, img_seq, output_file, timeout):
+	#number of t is randomly generated within [4,64]
+	num_t = random.randrange(4, 64)
 
 	#cmd = exe_name+' -i'+str(img_seq)+' -t'+str(num_t)
 	cmd = exe_name+' -t'+str(num_t)
@@ -84,24 +108,31 @@ def execute_paster(pu_exe, exe_name, img_seq, output_file, timeout):
 		sys.stdout.write('\tsuccess')
 		sys.stdout.flush()
 		output_file.log_exec(cmd, 'EXECUTION_SUCCESS')	
-	
-	if not os.path.exists("output.png"):
+
+	pngs = glob.glob('*.png')
+
+	if len(pngs) == 0:
 		print '\tno output'
-		output_file.log_cmp('NO OUTPUT')
+		output_file.log_cmp('NO_OUTPUT')
 		return
 
-	ret = png_comparator.compare_pngs("output.png", str(img_seq)+".png")
+	if len(pngs) > 1:
+		print '\tmultiple output'
+		output_file.log_cmp('MULTIPLE_OUTPUT')	
+		return
+
+	ret = png_comparator.compare_pngs(pngs[0], tester_root+'/'+str(img_seq)+".png")
 
 	if ret != True:
 		print '\tnot match!'
 		output_file.log_cmp('NOT MATCH')
+		copy_wrong_output( pngs[0], userid, exe_name )
 		return
 	else:
 		print '\tmatch!'
 		output_file.log_cmp('MATCH')
 
 	return
-
 
 
 if __name__ == '__main__':
